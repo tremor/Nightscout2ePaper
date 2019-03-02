4bcbeeb08e6b61caecbc2db1d2c097e10286d43a
tremor <martin2@web.de>
2019-03-02 09:27:49 +0100

    Update nightscout2epaper.bash

--- nightscout2epaper.bash
+++ nightscout2epaper.bash
@@ -1,12 +1,26 @@
 #!/bin/bash
 URL=https://YOURSITE.TLD/api/v1/entries/current.json
 CURRDATE=$(date '+%s')
+if [ -f lasthour.txt ];
+        then LASTHOUR=$(cat lasthour.txt);
+        else LASTHOUR=0;
+fi
 curl -s $URL |sed 's/,\"delta.*//' |sed -e 's/.*sgv\"://' > "value.txt"
 curl -s $URL |sed -e 's/.*direction\":\"//' |sed 's/\",\"type.*//' > "direction.txt"
 LASTVALUE=$(curl -s $URL |sed -e 's/.*date\"://'|cut -c -10)
 DIFF=$((CURRDATE-LASTVALUE))
 DIFF=$((DIFF/60))
+DIFFH=$((DIFF/60))
 LASTDATE=$(date -d @$LASTVALUE +%H:%M)
-echo "Vor" $DIFF "Min um" $LASTDATE > "timeinfo.txt"
 if [ $DIFF -gt 10 ]; then echo 1 > "old.txt"; else echo 0 > "old.txt"; fi
-python2 nightscout.py
+
+if [ $DIFF -lt 6 ]; then
+        echo "Vor" $DIFF "Min um" $LASTDATE > "timeinfo.txt"
+        python2 nightscout.py;
+else
+        if [ $DIFFH -gt $LASTHOUR ]; then
+                echo "Vor" $DIFFH "Stunden um" $LASTDATE > "timeinfo.txt"
+                python2 nightscout.py;
+        fi
+fi
+echo $DIFFH > "lasthour.txt"
